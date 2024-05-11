d920c7c0fc272819da79cad9aae5d94500a73e3b
Alpha0-1 <alphaomollo142@gmail.com>
2024-05-10 17:40:52 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
 
+"""
+This script fetches the status from https://alx-intranet.hbtn.io/status
+and displays the body of the response with information about it.
+"""
+
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
 
-# Fetching the URL content using urllib
-with urllib.request.urlopen(url) as response:
-    # Reading the response data
-    html_content = response.read().decode('utf-8')
+def fetch_status():
+    """
+    Fetches the status from a specified URL and displays the response details.
+    """
+    url = 'https://alx-intranet.hbtn.io/status'
 
-    # Displaying the body of the response with tabulation
-    print("- Body response:")
-    print("\t- type:", type(html_content))
-    print("\t- content:", html_content)
+    # Fetching the URL content using urllib
+    with urllib.request.urlopen(url) as response:
+        # Reading the response data
+        html_content = response.read().decode('utf-8')
+
+        # Displaying the body of the response with tabulation
+        print("- Body response:")
+        print("\t- type:", type(html_content))
+        print("\t- content:", html_content)
+
+
+if __name__ == "__main__":
+    fetch_status()
